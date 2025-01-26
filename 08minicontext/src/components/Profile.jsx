4220f95d449728380d70db342cd648cbd15cd2d9
Akash Dhiman <akashdhiman@Akashs-MacBook-Air.local>
2025-01-26 11:58:18 +0530

    mini Context using Context API

---
+++ 08minicontext/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
+
+
+function Profile(){
+
+    const {user} = useContext(UserContext);
+   
+        if(!user) return <div>Please log in</div>
+
+        return <div>Welcome {user.username}</div>
+
+    
+
+}
+
+export default Profile;
